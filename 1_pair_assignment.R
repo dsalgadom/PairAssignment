a2ee27742e3770cf832c0b4e1ede7cebb445bd72
dsalgadom <dsalgadom@hotmail.com>
2015-09-18 13:18:46 +0200

    Readme
    Prueba

---
+++ 1_pair_assignment.R
@@ -0,0 +1,14 @@
+#######################
+First Pair Assingnment ICSScDA
+Daniel Salgado-Moreno
+September-October 2015
+#######################
+
+#Find working directory
+getwd()
+list.files()
+setwd('Users/dsalgadom/HSoG/Introduction_to_Collaborative_Social_Science_Data_Analysis/git_repos')
+getwd()
+pwd()
+cd('Users/dsalgadom/HSoG/Introduction_to_Collaborative_Social_Science_Data_Analysis/git_repos')
+
