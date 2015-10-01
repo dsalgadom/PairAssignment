b9a3f9adde0db76b0205bf3db4fce3b8963b994d
dsalgadom <dsalgadom@hotmail.com>
2015-10-01 18:04:59 +0200

    Swiss Try 2
    R swiss dataset

---
+++ swiss try 2.R
@@ -0,0 +1,13 @@
+data(swiss)
+?swiss
+names(swiss)
+summary(swiss)
+
+#I will try some mathematical functions
+log(swiss$Examination)
+Examinationlog <- log(swiss$Examination)
+mean(Examinationlog)
+mean(Examinationlog, na.rm=T)
+is.na(Examinationlog)
+is.na(swiss$Fertility)
+
